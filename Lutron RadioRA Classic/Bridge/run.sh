7cac473b28f0b70d1e5ad696b05ccad98e39bf54
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-02-28 01:30:42 -0800

    more configuring the runtime

--- Lutron RadioRA Classic/Bridge/run.sh
+++ Lutron RadioRA Classic/Bridge/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-export TTY_SERIAL=/dev/tty.usbserial # update with /dev/tty* device connected to RadioRA hardware
-python radiora-classic-bridge.py
+export SERIAL_TTY=/dev/tty.usbserial # update with /dev/tty* device connected to RadioRA hardware
+
+# to run in Docker:
+docker run --env SERIAL_TTY=$SERIAL_TTY -t -i --device=$SERIAL_TTY radiora-classic-bridge
+
+# to run directly via the command line:
+#python radiora-classic-bridge.py
