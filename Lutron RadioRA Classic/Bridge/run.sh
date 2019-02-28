38070e0b921d729f359f843582e804d4877d8f76
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-02-28 01:46:50 -0800

    updated docs

--- Lutron RadioRA Classic/Bridge/run.sh
+++ Lutron RadioRA Classic/Bridge/run.sh
@@ -5,4 +5,6 @@ if [[ -z "$SERIAL_TTY" ]]; then
 fi
 echo "RadioRA Classic Bridge is using serial $SERIAL_TTY"
 
+# NOTE: mapping of Mac /dev/tty* into Linux Docker environment is currently not supported by Docker (Feb 2019)
+
 python3 radiora-classic-bridge.py
