88181c4be2570d5956c4c27da20ea66863732706
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-03-07 17:47:54 -0800

    added echo of cmd

--- Lutron RadioRA Classic/Bridge/docker-run.sh
+++ Lutron RadioRA Classic/Bridge/docker-run.sh
@@ -2,4 +2,8 @@
 
 # to run in Docker:
 #docker run --env SERIAL_TTY=$SERIAL_TTY -t -i --device=$SERIAL_TTY --privileged radiora-classic-bridge
-docker run -t -i --privileged radiora-classic-bridge
+
+CMD='docker run -t -i --privileged radiora-classic-bridge'
+
+echo "Running $CMD"
+$CMD
