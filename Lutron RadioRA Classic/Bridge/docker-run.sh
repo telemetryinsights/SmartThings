06d5624b9318dc2bcf9568a50669a77c2fab4aeb
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-05-28 10:36:48 -0700

    updated startup commands

--- Lutron RadioRA Classic/Bridge/docker-run.sh
+++ Lutron RadioRA Classic/Bridge/docker-run.sh
@@ -3,12 +3,9 @@
 # default to port 8333, if RADIORA_BRIDGE_PORT is not defined
 RADIORA_BRIDGE_PORT="${RADIORA_BRIDGE_PORT:-8333}"
 
-if [ -z "RADIORA_BRIDGE_TTY"]
-then
-  EXTRA_OPTIONS=""
-else
+if [ "$RADIORA_BRIDGE_TTY" != "" ]; then
   EXTRA_OPTIONS="--env RADIORA_BRIDGE_TTY=\"$RADIORA_BRIDGE_TTY\" --device=\"$RADIORA_BRIDGE_TTY\""
-end
+fi
 
 CMD="docker run --privileged -t -i $EXTRA_OPTIONS radiora-classic-bridge"
 echo "Executing: $CMD"
