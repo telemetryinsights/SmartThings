6fb33608c8840464ca01e7fe8cf3ba441da38a18
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-05-28 10:07:11 -0700

    updated docker startup notes

--- Lutron RadioRA Classic/Bridge/docker-run.sh
+++ Lutron RadioRA Classic/Bridge/docker-run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # to run in Docker:
-#docker run --env SERIAL_TTY=$SERIAL_TTY -t -i --device=$SERIAL_TTY --privileged radiora-classic-bridge
+#CMD="docker run --env SERIAL_TTY=$RADIORA_BRIDGE_TTY -t -i --device=$RADIORA_BRIDGE_TTY --privileged radiora-classic-bridge"
+CMD="docker run -t -i --privileged radiora-classic-bridge"
 
-CMD='docker run -t -i --privileged radiora-classic-bridge'
-
-echo "Running $CMD"
+echo "Executing: $CMD"
 $CMD
