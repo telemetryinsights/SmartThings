851c6b2728cd9a1010fb25afea139f2ed12210ae
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-01-30 09:08:09 -0800

    log updates

--- Lutron RadioRA Classic/Bridge/lutron/serial.py
+++ Lutron RadioRA Classic/Bridge/lutron/serial.py
@@ -4,7 +4,7 @@ import serial
 import sys
 import logging
 
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 # unless an explicit RADIORA_BRIDGE_TTY is specified to use, this searches by default across
 # a set of common TTYs to find a RadioRA Classic RS232 module
@@ -37,17 +37,17 @@ class RadioRASerial:
         # NOTE: RADIORA_BRIDGE_TTY environment variable overrides the default search paths
         if 'RADIORA_BRIDGE_TTY' in os.environ:
             tty_config = os.environ['RADIORA_BRIDGE_TTY']
-            log.info(">>>>> RadioRA Classic device search paths overridden by env variable RADIORA_BRIDGE_TTY=" + tty_config)
+            LOG.info(">> RadioRA Classic device search paths overridden by env variable RADIORA_BRIDGE_TTY=" + tty_config)
             ttys_to_search = ','.split(tty_config)
 
         self.__discover_radiora_serial__(ttys_to_search)
 
     def __discover_radiora_serial__(self, ttys_to_search):
-        log.info(">>>>> Discovering RadioRA device on serial interfaces: {}".format(', '.join(ttys_to_search)))
+        LOG.info(">>>>> Discovering RadioRA device on serial interfaces: {}".format(', '.join(ttys_to_search)))
         for tty in ttys_to_search:
             try:
                 if not os.path.exists(tty):
-                    log.info(">>>>> Serial device {} does not exist, ignoring".format(tty))
+                    LOG.info(">> Serial device {} does not exist, ignoring".format(tty))
                     continue
                 
                 self.serial = serial.Serial(tty,
@@ -64,15 +64,15 @@ class RadioRASerial:
                 if ((response != None) and response.startswith('REV,')):
                     self.version = response.lstrip('REV,')
                     self.tty = tty
-                    log.warning('>>>>> Discovered Lutron RadioRA Classic at {} (version={})'.format(self.tty,self.version))
-                    print('>>>>> Discovered Lutron RadioRA Classic at {} (version={})'.format(self.tty,self.version))
+                    LOG.warning('>> Discovered Lutron RadioRA Classic at {} (version={})'.format(self.tty,self.version))
+                    print('>> Discovered Lutron RadioRA Classic at {} (version={})'.format(self.tty,self.version))
                     break
 
                 self.serial.close()
                 self.serial = None
 
             except:
-                log.error('Unexpected error: %s', sys.exc_info()[0])
+                LOG.error('Unexpected error: %s', sys.exc_info()[0])
                 raise RuntimeError("No RadioRA RS232 devices discovered at {}".format(', '.join(ttys_to_search)))
         
         if self.version == None:
@@ -97,7 +97,7 @@ class RadioRASerial:
         return bytes(line).decode('utf-8')
 
     def writeCommand(self, command):
-        log.debug('>>>>> Serial write: {}'.format(command))
+        LOG.debug('>> Serial write: {}'.format(command))
         self.serial.reset_input_buffer()
         self.serial.write((command + "\r\n").encode('utf-8'))
 
@@ -111,5 +111,5 @@ class RadioRASerial:
 #            result = result.bytes.decode('utf-8').upper()
         end = time.time()
 
-        log.debug('>>>>> Serial read ({1:.0f} ms): {0}'.format(result, 1000 * (end-start)))
+        LOG.debug('>> Serial read ({1:.0f} ms): {0}'.format(result, 1000 * (end-start)))
         return result
